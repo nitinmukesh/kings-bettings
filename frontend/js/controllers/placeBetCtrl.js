8a1bb31bfac4004c965b79610e0f3fc6d85e48d6
prajaktakamble <prajakta.kamble@wohlig.com>
2018-05-03 11:08:02 +0530

    sidemenu

--- frontend/js/controllers/placeBetCtrl.js
+++ frontend/js/controllers/placeBetCtrl.js
@@ -1,6 +1,7 @@
 myApp.controller('PlaceBetCtrl', function ($scope, TemplateService, NavigationService) {
     $scope.template = TemplateService.getHTML("content/place-bet.html");
     TemplateService.title = "Place Bet"; //This is the Title of the Website
+    TemplateService.sidemenu2 = "";
     $scope.navigation = NavigationService.getNavigation();
 
 });
