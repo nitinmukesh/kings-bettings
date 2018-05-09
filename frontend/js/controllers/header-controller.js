df9fb883aa1fe4c246d1fb795f0ed5976b6caff9
Ganesh Kothavale <ganesh.wohlig@gmail.com>
2018-05-09 10:51:09 +0530

    done sidemenu

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -1,4 +1,4 @@
-myApp.controller('headerCtrl', function ($scope, TemplateService) {
+myApp.controller('headerCtrl', function ($scope, TemplateService, $state) {
     $scope.template = TemplateService;
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         $(window).scrollTop(0);
@@ -9,4 +9,8 @@ myApp.controller('headerCtrl', function ($scope, TemplateService) {
         isFirstOpen: true,
         isFirstDisabled: false
     };
+    $scope.currentUrl = $state.current.name;
+    $scope.goback = function () {
+        window.history.back();
+    }
 });
