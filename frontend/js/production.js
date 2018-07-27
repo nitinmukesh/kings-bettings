b844aacf88287aa6945985369c461934fd7f818c
sohan1 <shn619@gmail.com>
2018-07-27 12:09:29 +0530

    production

--- frontend/js/production.js
+++ frontend/js/production.js
@@ -86313,12 +86313,12 @@ myApp.service('jStorageService', function () {
 });
 // var adminurl = adminUUU;
 // adminurl = adminurl + "/api/";
-adminUUU = "http://rates.kingplay.online/"; //socket betfair
-sportsSocket = "http://sportsbookb.kingplay.online/";
+adminUUU = "https://rates.kingsplay.co/"; //socket betfair
+sportsSocket = "https://sportsbookb.kingsplay.co/";
 // sportsSocket = "http://192.168.2.30:1337/";
 // sportsSocket = "http://192.168.43.8:1337/";
 adminurl = sportsSocket + "api/"; //sports book
-mainServer = "http://kingplay.online/"; //main server
+mainServer = "https://test.kingsplay.co/"; //main server
 // mainServer = "http://192.168.2.31:1337/"; //main server
 // adminurl = "http://192.168.1.107:1337/api/"
 io.sails.url = adminUUU;
