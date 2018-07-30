15d09122b142867d8aa1dd1336ab57ddae10fa1a
sohan1 <shn619@gmail.com>
2018-07-30 13:23:37 +0530

    my bet

--- frontend/views/content/mybet/mybet.js
+++ frontend/views/content/mybet/mybet.js
@@ -2,14 +2,22 @@ myApp.controller('MybetCtrl', function ($scope, TemplateService, NavigationServi
     $scope.template = TemplateService.getHTML("content/mybet/mybet.html");
     TemplateService.title = "My Bet"; //This is the Title of the Website
     TemplateService.sidemenu2 = "";
+    TemplateService.rightsidemenu = "";
     $scope.navigation = NavigationService.getNavigation();
 
 
 
     $scope.getAccountStatement = function (data) {
         NavigationService.getAccountStatement(data, function (data) {
-            $scope.accountStatement = data.data.result.accountStatement;
-            console.log("$scope.accountStatement", $scope.accountStatement);
+            console.log("data.data.error",data.data.error);
+            console.log("data.data.error",data.data);
+            if (data.data.result.accountStatement) {
+                $scope.accountStatement = data.data.result.accountStatement;
+            } else if (data.data.error) {
+                $scope.error = data.data.error;
+            } else {
+                $scope.error = "No data Found";
+            }
         });
     };
     $scope.getAccountStatement();
